10761f3a56b4af7068f35fd69feadc6e49e21f01
Chris Russell <crussell@sonnysdirect.com>
2018-12-05 19:58:15 -0500

    Allow arcade drive implementations provide custom quick turn triggers
    Since quick turn triggers may be based off of throttle and/or turn
    values, everything is now provided using a single method call.
    
    Any decisions on how the quick turn should behave based on the current
    input values is still in the hands of the base class. This detail is
    bound to the physical attributes of the Robot and should not vary based
    on inputs schemes.

--- src/main/java/org/team1251/frc/robot/humanInterface/input/ArcadeDriveInput.java
+++ src/main/java/org/team1251/frc/robot/humanInterface/input/ArcadeDriveInput.java
@@ -2,34 +2,57 @@ package org.team1251.frc.robot.humanInterface.input;
 
 public abstract class ArcadeDriveInput implements DriveInput {
 
+    class InputValues {
+        final double throttle;
+        final double turn;
+        final boolean isQuickTurn;
+
+        InputValues(double throttle, double turn, boolean isQuickTurn) {
+            this.throttle = throttle;
+            this.turn = turn;
+            this.isQuickTurn = isQuickTurn;
+        }
+    }
+
+    InputValues getInputValues(HumanInput humanInput) {
+
+        // Get the direct throttle/turn inputs
+        double throttle = getThrottleInput(humanInput);
+        double turn = getTurnInput(humanInput);
+
+        // If throttle is very low, trigger a quick-turn
+        boolean isQuickTurn = Math.abs(throttle) <= .05;
+
+        // Put it all together and return it.
+        return new InputValues(throttle, turn, isQuickTurn);
+    }
 
     abstract double getThrottleInput(HumanInput humanInput);
     abstract double getTurnInput(HumanInput humanInput);
 
     @Override
     public DrivePower getDrivePower(HumanInput humanInput) {
-        double throttleInput = getThrottleInput(humanInput);
-        double turnInput = getTurnInput(humanInput);
+        InputValues input = getInputValues(humanInput);
 
-        // If near to no throttle, do an in-place turn using the turn input. This fixes a problem where
-        // some noise on the line will rarely prevent the quick-turn logic from triggering.
-        if (Math.abs(throttleInput) <= .05) {
-            return new DrivePower(-turnInput, turnInput);
+        // Ignore throttle in quick-turn scenarios.
+        if (input.isQuickTurn) {
+            // TODO: consider adjusting based on current throttle and/or current velocity (not currently available)
+            return new DrivePower(-input.turn, input.turn);
         }
 
         // Calculate the dampened throttle. If the original input was negative, negate the dampened value.
-        double dampenedThrottle = calculateTurnThrottle(Math.abs(throttleInput), Math.abs(turnInput));
-        if (throttleInput < 0) {
+        double dampenedThrottle = calculateTurnThrottle(Math.abs(input.throttle), Math.abs(input.turn));
+        if (input.throttle < 0) {
             dampenedThrottle = -dampenedThrottle;
         }
 
 
 
-        double leftPower = throttleInput;
-        double rightPower = throttleInput;
+        double leftPower = input.throttle;
+        double rightPower = input.throttle;
 
 
-        if (turnInput < 0) {
+        if (input.turn < 0) {
             // Throttle left drive
             leftPower = dampenedThrottle;
         } else {
